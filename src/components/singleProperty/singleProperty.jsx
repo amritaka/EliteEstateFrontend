1ff3e24fcc0f445594c643504b96f8adf120a66a
amritaka <158139181+amritaka@users.noreply.github.com>
2025-05-13 12:21:17 +0530

    Update singleProperty.jsx

--- src/components/singleProperty/singleProperty.jsx
+++ src/components/singleProperty/singleProperty.jsx
@@ -111,6 +111,10 @@ export default function PropertyListing() {
                           Amount Offered
                           <p>Rs {d?.amountBid}</p>
                         </label>
+                         <label>
+                          Message
+                          <p>Rs {d?.message}</p>
+                        </label>
                       </div>
                       <div className="contact">
                         <label>
